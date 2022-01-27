e6c534d74a210aeae47c1091da38fa2baa0672d3
FlpSlva <felipinhoznoroeste@gmail.com>
2022-01-27 10:22:30 -0300

    assembling complete user model

--- src/modules/accounts/entity/User.ts
+++ src/modules/accounts/entity/User.ts
@@ -2,10 +2,23 @@ import {v4 as uuidv4} from 'uuid';
 
 
 
-
-
 class User {
 
+    id?: string
+
+    name: string
+
+    password: string
+
+    email: string
+
+    created_at: string
+
+    constructor(){
+        if(!this.id){
+            this.id = uuidv4()
+        }
+    }
 }
 
 export {User}
