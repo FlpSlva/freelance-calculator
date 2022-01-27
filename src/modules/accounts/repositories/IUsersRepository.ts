5d0c5ac93983cda4c2c796c063284297595d7410
FlpSlva <felipinhoznoroeste@gmail.com>
2022-01-27 10:38:40 -0300

    feat: creating user repository parameters

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -8,7 +8,8 @@
 
 interface IUsersRepository {
 
-    
-
+    create(data: ICreateUserDto): Promise<void>;
+    findByEmail(email: string): Promise<User>;
+    findById(id: string): Promise<User>;
 
 }
