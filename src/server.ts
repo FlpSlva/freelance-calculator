e8be3199a0060305bdc0924f4b540b5b52c6a946
FlpSlva <felipinhoznoroeste@gmail.com>
2022-01-29 02:13:25 -0300

    feat: created middleware of appError on server

--- src/server.ts
+++ src/server.ts
@@ -14,4 +14,21 @@ const port = 3333;
 app.use(express.json())
 app.use(router);
 
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+
+    if(err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            message: err.message
+        })
+    }
+
+    
+    return res.status(500).json({
+        status: "error",
+        message: `Internal Server Error - ${err.message}`
+    })
+
+})
+
 app.listen(port, () => console.log("server is running on port", port));
